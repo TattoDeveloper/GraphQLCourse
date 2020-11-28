c9829ab9b8616ea311a4da2e52afd4acd31e4de7
TattoDeveloper <jrtatto@gmail.com>
2020-11-27 19:56:29 -0500

    ddd

--- course/src/miniTwitter/app.js
+++ course/src/miniTwitter/app.js
@@ -8,13 +8,15 @@ let twitts = [{
     id:1,
     text: "Texto de prueba",
     date: new Date(),
-    user: 1
+    user: 1,
+    feed:[1,2,3],
 },
 {
     id:2,
     text: "Texto de prueba 2",
     date: new Date(),
-    user: 1
+    user: 1,
+    feed:[1,2,3]
 }
 ]
 let users = [{
@@ -32,7 +34,50 @@ let users = [{
 
 ]
 
+let comments =[
+    {
+      id:1,
+      text: "comentario 1",
+      date: Date.now()
+    },
+    {
+        id:2,
+        text: "comentario 2",
+        date: Date.now()
+    },
+    {
+        id:3,
+        text: "comentario 3",
+        date: Date.now()
+    },
+    {
+        id:4,
+        text: "comentario 4",
+        date: Date.now()
+    }
+]
+
+let likes =[
+    {
+        id:1,
+        twitt:1,
+        date: Date.now()
+    },
+    {
+        id:2,
+        twitt:2,
+        date: Date.now()
+    },
+    {
+        id:3,
+        twitt:2,
+        date: Date.now()
+    }
+]
+
 const typeDefs = gql`
+    union Feed = Like | Comment
+
   type Query{
     totalTwitts:Int!,
     allTwitts:[Twitt!]!,
@@ -58,7 +103,21 @@ const typeDefs = gql`
     text: String!,
     date: Date,
     user: User!
+    feed:[Feed!]
  }
+
+ type Like{
+    id: ID!,
+    twitt: Twitt,
+    date: Date!
+  }
+ 
+  type Comment{
+    id: ID!,
+    text: String!,
+    twitt: Twitt,
+    date: Date!
+  }
  scalar Date
 `
 const resolvers = {
@@ -71,8 +130,19 @@ const resolvers = {
         twits:parent=> twitts.filter(twitt => parent.twits.includes(twitt.id))
     },
     Twitt:{
-        user: parent=> users.find(user => user.id == parent.user)
+        user: parent=> users.find(user => user.id == parent.user),
+        feed: parent=> {
+           return likes.filter(like => like.twitt == parent.id)
+                 .concat(comments.filter(comment => comment.id == parent.id))
+        }
     },
+    Feed:{
+        __resolveType(obj,_){
+           console.log(obj,'objec')
+           return obj.text ? 'Comment' : 'Like'
+        }
+    },
+
     Mutation:{
         createTwitt:(parent, payload)=>{
               const newTwitt ={
